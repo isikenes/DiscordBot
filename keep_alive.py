cc20ae6d869136de6ebb6800cda3b31a6f1feb78
Enes Işık <30756694+isikenes@users.noreply.github.com>
2024-04-04 19:28:07 +0300

    too many req why

--- keep_alive.py
+++ keep_alive.py
@@ -5,11 +5,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Alive"
+  return "Alive"
 
 def run():
   app.run(host='0.0.0.0',port=8080)
 
 def keep_alive():  
-    t = Thread(target=run)
-    t.start()
+  t = Thread(target=run)
+  t.start()
